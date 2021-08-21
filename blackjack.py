cf9fc57497bccc80d76d5e5a28db2f50ec11f336
fander2468 <86483561+fander2468@users.noreply.github.com>
2021-08-21 15:36:37 -0400

    second commit

--- blackjack.py
+++ blackjack.py
@@ -12,49 +12,117 @@
 # 4. If the dealer deals the Player cards equal to 21 on the first deal, the Player wins. This is referred to as Blackjack. Blackjack is NOT the same as getting cards that equal up to 21 after the first deal. Blackjack can only be attained on the first deal.
 
 # 5. The Player will never see the Dealer's hand until the Player chooses to 'stand'. A Stand is when the player tells the dealer to not deal it anymore cards. Once the player chooses to Stand, the Player and the Dealer will compare their hands. Whoever has the higher number wins. Keep in mind that the Dealer can also bust.
-from random import shuffle
+
+# Imports that will be needed for the app
+import random 
 import os
 
+# Player class
 class Player():
-    def __init__(self, player, dealer):
-        self.player = player
-        self.dealer = dealer
+    def __init__(self, player_cards = [], dealer_cards = []):
+        self.player_cards = player_cards
+        self.dealer_cards = dealer_cards
 
+    # player cards 
+    def player(self):
+        # self.player_cards = getattr(Card, self.player_cards)
+        self.player_cards = Cards.deal_two_cards(self)
+
+    # dealer cards
+    def dealer(self):
+        # self.dealer_cards = getattr(Card, self.dealer_cards)
+        self.dealer_cards = Cards.deal_two_cards(self)
 
     # player decisons method - player can hit, stand
-    def player(self):
-        pass
+    def player_decision(self, player_input):
+        if player_input == 'hit'.lower() and Cards.count > 0:
+            self.player_cards = Cards.deal_one_card(self)
+            return True
+        elif player_input == 'stay'.lower():
+            print("Player decided to stay and doesn't want anymore cards'.")
+
+    def dealer_decison(self, player_input):
+        if sum(self.dealer_cards) < 17 and player_input == 'stay'.lower():
+            self.dealer_cards.append(Cards.deal_one_card())
+        
+    # method to work with face cards 
+    def card_face(self):
+        for i in self.player_cards:
+            if i == 'jack' or i == 'queen' or i == 'king':
+                self.player_cards[i] = 10
+
+    # method to work with ace card 
+    def card_ace(self):
+        for i in self.player_cards:
+            if i == 'ace' and sum(self.player_cards) >= 11:# Need to fix this line
+                self.player_cards[i] = 1
+            else:
+                self.player_cards[i] = 11
+
+ 
 
 
-    # dealers decisions method - dealer can draw a card 
-    def dealer(self):
-        pass
 
 
+
+
+
+
+# Card class
 class Cards():
-    def __init__(self, cards):
+    def __init__(
+        self,
+        cards = ["ace" ,2 ,3 ,4 ,5 ,6 ,7 ,8 ,9 ,10 ,"jack" ,"queen" ,"king"],
+        card_suit = ["clubs", "hearts", "diamonds", "spades"],
+        cards_choices = [], count = 52):
         self.cards = cards
+        self.cards_suit = card_suit
+        self.cards_choices = cards_choices
+        self.count = count
 
+    
 
-    # cards can be shuffled randomly
+    # cards/list can be shuffled randomly each time a new game is played 
     def shuffled(self):
-        pass
+        random.shuffle(self.cards)
+
+    # deal two cards from the deck after cards are shuffled to both player and dealer
+    def deal_two_cards(self):
+        for i in range(2):
+            self.cards_choices.random.choice(self.cards)
+
+    # deal one card from the deck each time a hit is called
+    def deal_one_card(self):
+        for i in range(1):
+            self.cards_choices.append(random.choice(self.cards))
+
+    def remove_card_from_deck(self):
+        if Player.player_decision == True:
+            self.count -= 1
+    
 
 
-    # deal two cards from the deck 
-    def deal_cards(self):
-        pass
+
+    
 
 
-    # show method for cards can see both of players cards but one of dealers
-    def show(self):
-        pass
+
 
 
 class Board():
     def __init__(self, board):
         self.board = board
-
     
-   
+    def start_game(self):
+        pass
+
+    # show method for cards; can compare dealer with player
+    def show(self):
+        pass
+    
+    def win(self):
+       pass
+
+    def turn(self):
+       pass    
 
